77c6f84c8ea38bd61c91924f1cdc1f7c357a9e82
cyardimci <cyardimci@sabanciuniv.edu>
2016-10-25 23:14:32 +0300

    #518 oyun klasorunde ana-ekran kod modernizasyonu

--- client/ana-ekran/ana-ekran.js
+++ client/ana-ekran/ana-ekran.js
@@ -1,6 +1,5 @@
 Template.anaEkran.onCreated(function() {
-  var template = this;
-  template.autorun(function() {
-    template.subscribe('girilmemisSinavVar', moment(TimeSync.serverTime(null, 5 * 60 * 1000)).toDate());
+  this.autorun(() => {
+    this.subscribe('girilmemisSinavVar', moment(TimeSync.serverTime(null, 5 * 60 * 1000)).toDate());
   })
 });
