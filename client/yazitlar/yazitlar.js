6eb3d21a715ad61b275b78c8c57f9877d4cc08f4
cyardimci <cyardimci@sabanciuniv.edu>
2016-10-26 00:23:05 +0300

    #518 oyun klasorunde yazitlar  kod modernizasyonu

--- client/yazitlar/yazitlar.js
+++ client/yazitlar/yazitlar.js
@@ -1,43 +1,41 @@
 Template.yazitlar.onCreated(function() {
-  var template = this;
 
-  template.aktifDers = new ReactiveVar();
+  this.aktifDers = new ReactiveVar();
 
-  template.subscribe('mufredatlar', function() {
-    Tracker.afterFlush(function() {
-      var ilkDers = M.C.Dersler.findOne({}, {sort: {createdAt: 1}});
-      template.aktifDers.set(ilkDers._id);
+  this.subscribe('mufredatlar', () => {
+    Tracker.afterFlush(() => {
+      const ilkDers = M.C.Dersler.findOne({}, {sort: {createdAt: 1}});
+      this.aktifDers.set(ilkDers._id);
     })
   });
-  template.subscribe('fsdersicerik');
+  this.subscribe('fsdersicerik');
 });
 
 Template.yazitlar.onRendered(function() {
-  var template = this;
-  template.mufredatKaydirGerekli = new ReactiveVar(false);
-  template.autorun(function(){
-    var aktifDers = template.aktifDers.get();
+  this.mufredatKaydirGerekli = new ReactiveVar(false);
+  this.autorun(() => {
+    const aktifDers = this.aktifDers.get();
     if (aktifDers) {
-      Tracker.afterFlush(function(){
-        template.mufredatKaydirGerekli.set(template.$('.mufredatWrapper > div').height() > template.$('.mufredatWrapper').innerHeight());
+      Tracker.afterFlush(() => {
+        this.mufredatKaydirGerekli.set(this.$('.mufredatWrapper > div').height() > this.$('.mufredatWrapper').innerHeight());
       })
     }
   })
 });
 
 Template.yazitlar.helpers({
-  dersler: function() {
-    var derslerCursor = M.C.Dersler.find({}, {sort: {createdAt: 1}});
+  dersler() {
+    const derslerCursor = M.C.Dersler.find({}, {sort: {createdAt: 1}});
     return derslerCursor.count() && derslerCursor;
   },
-  dersKaydirGerekli: function() {
+  dersKaydirGerekli() {
     return M.C.Dersler.find().count() > 6;
   },
-  mufredat: function() {
-    var aktifDers = Template.instance().aktifDers.get();
-    var aktifEgitimYili = M.C.AktifEgitimYili.findOne();
+  mufredat() {
+    const aktifDers = Template.instance().aktifDers.get();
+    const aktifEgitimYili = M.C.AktifEgitimYili.findOne();
     if (aktifDers && aktifEgitimYili) {
-      var mufredat = M.C.Mufredat.findOne({
+      const mufredat = M.C.Mufredat.findOne({
         kurum: Meteor.user().kurum,
         sinif: Meteor.user().sinif,
         egitimYili: aktifEgitimYili.egitimYili,
@@ -48,41 +46,41 @@ Template.yazitlar.helpers({
       return false;
     }
   },
-  mufredatKaydirGerekli: function() {
+  mufredatKaydirGerekli() {
     return Template.instance().mufredatKaydirGerekli.get();
   }
 });
 
-var dersIcerikView;
+let dersIcerikView;
 
 Template.yazitlar.events({
-  'click .muhurGrup': function(e,t) {
+  'click .muhurGrup'(e,t) {
     t.aktifDers.set(this._id);
   },
-  'click .sol': function(e,t) {
+  'click .sol'(e,t) {
     t.$('.muhurGruplariWrapper').animate({
       scrollLeft: '-=128'
     }, 300);
   },
-  'click .sag': function(e,t) {
+  'click .sag'(e,t) {
     t.$('.muhurGruplariWrapper').animate({
       scrollLeft: '+=128'
     }, 300);
   },
-  'click .yukari': function(e,t) {
+  'click .yukari'(e,t) {
     t.$('.mufredatWrapper').animate({
       scrollTop: '-=128'
     }, 300);
   },
-  'click .asagi': function(e,t) {
+  'click .asagi'(e,t) {
     t.$('.mufredatWrapper').animate({
       scrollTop: '+=128'
     }, 300);
   },
-  'click [data-trigger="openDersIcerikModal"]' : function(e,t) {
+  'click [data-trigger="openDersIcerikModal"]'(e,t) {
     e.preventDefault();
-    var icerik = this;
-    var data = {
+    const icerik = this;
+    const data = {
       fileName: icerik.name(),
       url: icerik.url({auth: false})
     };
@@ -94,11 +92,11 @@ Template.yazitlar.events({
 
 Template.dersIcerikModal.onRendered(function() {
   PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
-  var modalWidth = $('#pdfcontainer').width();
-  var modalHeight = $('#pdfcontainer').height();
+  const modalWidth = $('#pdfcontainer').width();
+  const modalHeight = $('#pdfcontainer').height();
 
-  var url = this.data.url;
-  var pdfDoc = null,
+  const url = this.data.url;
+  let pdfDoc = null,
     pageNum = 1,
     pageRendering = false,
     pageNumPending = null,
@@ -108,19 +106,19 @@ Template.dersIcerikModal.onRendered(function() {
 
   function renderPage(num) {
     pageRendering = true;
-    pdfDoc.getPage(num).then(function(page) {
-      var viewport = page.getViewport(1);
-      var ws = modalWidth / viewport.width;
-      var hs = modalHeight / viewport.height;
+    pdfDoc.getPage(num).then(page => {
+      let viewport = page.getViewport(1);
+      const ws = modalWidth / viewport.width;
+      const hs = modalHeight / viewport.height;
       scale = hs < ws ? hs : ws;
       viewport = page.getViewport(scale);
 
       canvas.height = viewport.height;
       canvas.width = viewport.width;
 
-      var renderTask = page.render({canvasContext: ctx, viewport: viewport});
+      const renderTask = page.render({canvasContext: ctx, viewport: viewport});
 
-      renderTask.promise.then(function () {
+      renderTask.promise.then(() => {
         pageRendering = false;
         if (pageNumPending !== null) {
           renderPage(pageNumPending);
@@ -158,7 +156,7 @@ Template.dersIcerikModal.onRendered(function() {
   }
   document.getElementById('next').addEventListener('click', onNextPage);
 
-  PDFJS.getDocument(url).then(function (pdfDoc_) {
+  PDFJS.getDocument(url).then(pdfDoc_ => {
     pdfDoc = pdfDoc_;
     document.getElementById('page_count').textContent = pdfDoc.numPages;
     renderPage(pageNum);
@@ -167,7 +165,7 @@ Template.dersIcerikModal.onRendered(function() {
 });
 
 Template.dersIcerikModal.events({
-  'click [data-trigger="closeDersIcerikModal"]': function(e,t) {
+  'click [data-trigger="closeDersIcerikModal"]'(e,t) {
     e.preventDefault();
     Blaze.remove(dersIcerikView);
   }
